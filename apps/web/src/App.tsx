a8d8e234c26e2984623b2de0bfb5a4c328622fca
Brian Ephraim <brianephraim@gmail.com>
2026-02-11 14:41:18 -0500

    get rid of Homepage

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 
 import BenchmarkPage from './pages/BenchmarkPage';
-import HomePage from './pages/HomePage';
 import PermutationsPage from './pages/PermutationsPage';
 
 export default function App() {
@@ -9,15 +8,13 @@ export default function App() {
     <HashRouter>
       <div style={{ maxWidth: 900, margin: '32px auto', padding: 16 }}>
         <nav style={{ display: 'flex', gap: 12, alignItems: 'center', marginBottom: 18 }}>
-          <Link to="/">Demo</Link>
+          <Link to="/">Permutations</Link>
           <Link to="/benchmark">Benchmark</Link>
-          <Link to="/permutations">Permutations</Link>
         </nav>
 
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<PermutationsPage />} />
           <Route path="/benchmark" element={<BenchmarkPage />} />
-          <Route path="/permutations" element={<PermutationsPage />} />
         </Routes>
       </div>
     </HashRouter>
