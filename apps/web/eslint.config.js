5be1b1ca8fbff4bf33fcf650e5c39f08563978d2
Brian Ephraim <brianephraim@gmail.com>
2026-05-24 16:31:50 -0400

    Set ESLint tsconfig root dirs

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -4,6 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const tsconfigRootDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   globalIgnores(['dist', 'public/snack-src']),
@@ -48,6 +52,7 @@ export default defineConfig([
         ecmaFeatures: {
           jsx: true,
         },
+        tsconfigRootDir,
       },
       globals: globals.browser,
     },
--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -3,6 +3,10 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const tsconfigRootDir = dirname(fileURLToPath(import.meta.url));
 
 export default [
   { ignores: ['dist/**'] },
@@ -13,6 +17,9 @@ export default [
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      parserOptions: {
+        tsconfigRootDir,
+      },
       globals: globals.browser,
     },
     plugins: {
